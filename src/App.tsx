e681c8de10c3eb2a049817107dcfe92757582756
merouane <ikkisauto@gmail.com>
2024-06-09 12:13:53 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Nav from "./navbar"
+import Hero from "./hero"
+const App = () => {
+  return (
+    
+    <>
+    <Nav></Nav>
+    <Hero></Hero>
+    
+    </>
+      
+      
+   
+  )
+}
+
+export default App
